15d22c31ceb1bc4b9fc0b84add0a21a2e10de8b1
Bong-soo Kim <mozartk30@gmail.com>
2018-09-15 15:55:20 +0900

    add Exceptions

---
+++ src/Exceptions/FFProbeNotFoundException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace mozartk\GetKeyFrames\Exceptions;
+
+
+class FFProbeNotFoundException extends \Exception
+{
+
+}
--- src/GetKeyFrames.php
+++ src/GetKeyFrames.php
@@ -2,11 +2,15 @@
 
 namespace mozartk\GetKeyFrames;
 
+use mozartk\GetKeyFrames\Exceptions\FFProbeNotFoundException;
+use mozartk\GetKeyFrames\Exceptions\VideoNotFoundException;
 use mozartk\GetKeyFrames\Parser\CsvParser;
 use Symfony\Component\Process\Process;
 
 class GetKeyFrames
 {
+    const DEFAULT_FFPROBE_PATH = '/usr/local/bin/ffprobe';
+
     /**
      * @var object 데이터를 파싱하는 파서의 인스턴스
      */
@@ -22,6 +26,10 @@ class GetKeyFrames
      * @var mixed
      */
     protected $process;
+
+    protected $ffprobePath;
+
+    protected $videoPath;
 
 
     public function __construct($opt = array())
@@ -42,22 +50,58 @@ class GetKeyFrames
         $process = new \mozartk\GetKeyFrames\Binary\Process();
         $process->initProcess();
 
-        $process->setBinaryPath('/usr/local/bin/ffprobe');
         $process->setArgs(array('-show_frames', '-select_streams', 'v', '-show_entries',
             'frame=coded_picture_number,pict_type,pkt_dts_time', '-print_format', 'csv'));
 
         $this->process = $process;
     }
 
+    public function setFFProbePath($ffprobePath)
+    {
+        if(!file_exists($ffprobePath)) {
+            throw new FFProbeNotFoundException("FFProbe not found.");
+        }
+
+        $this->ffprobePath = $ffprobePath;
+    }
+
+    public function getFFProbePath()
+    {
+        if(trim($this->ffprobePath) == '') {
+            $this->setFFProbePath(self::DEFAULT_FFPROBE_PATH);
+        }
+
+        return $this->ffprobePath;
+    }
+
+    private function setVideoPath($videoPath)
+    {
+        if(!file_exists($videoPath)) {
+            throw new VideoNotFoundException('videofile not found.');
+        }
+
+        $this->videoPath = $videoPath;
+    }
+
+    private function getVideoPath()
+    {
+        return $this->videoPath;
+    }
+
     /**
      * 동영상의 키 정보를 추출함.
      *
-     * @param string $filePath
+     * @param string $videoPath
      * @return array
      */
-    public function getVideoInfo(string $filePath)
+    public function getVideoInfo(string $videoPath)
     {
-        $this->process->setVideoPath($filePath);
+        $ffprobe   = $this->getFFProbePath();
+        $this->setVideoPath($videoPath);
+        $videoPath = $this->getVideoPath();
+
+        $this->process->setBinaryPath($ffprobe);
+        $this->process->setVideoPath($videoPath);
 
         $this->process->runProcess();
         $data = $this->process->getOutput();
@@ -71,9 +115,14 @@ class GetKeyFrames
      * @param string $filePath
      * @return array
      */
-    public function getFrame(string $filePath)
+    public function getFrame(string $videoPath)
     {
-        $this->process->setVideoPath($filePath);
+        $ffprobe   = $this->getFFProbePath();
+        $this->setVideoPath($videoPath);
+        $videoPath = $this->getVideoPath();
+
+        $this->process->setBinaryPath($ffprobe);
+        $this->process->setVideoPath($videoPath);
 
         $this->process->runProcess();
         $data = $this->process->getOutput();
@@ -87,9 +136,14 @@ class GetKeyFrames
      * @param string $filePath
      * @return array
      */
-    public function getTime(string $filePath)
+    public function getTime(string $videoPath)
     {
-        $this->process->setVideoPath($filePath);
+        $ffprobe   = $this->getFFProbePath();
+        $this->setVideoPath($videoPath);
+        $videoPath = $this->getVideoPath();
+
+        $this->process->setBinaryPath($ffprobe);
+        $this->process->setVideoPath($videoPath);
 
         $this->process->runProcess();
         $data = $this->process->getOutput();
